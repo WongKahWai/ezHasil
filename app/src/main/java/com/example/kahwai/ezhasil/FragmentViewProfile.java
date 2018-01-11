aede4d5d66bdee0e63a6c916fdd0cdbe2f7691d1
Wern Min <grayx3@hotmail.com>
2018-01-11 16:49:04 +0800

    saf

--- app/src/main/java/com/example/kahwai/ezhasil/FragmentViewProfile.java
+++ app/src/main/java/com/example/kahwai/ezhasil/FragmentViewProfile.java
@@ -6,18 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
 
 
 public class FragmentViewProfile extends Fragment {
     public FragmentViewProfile() { }
 
-
+    private DatabaseReference mDatabase;
     private View rootView;
+
+    //private TextView
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_view_profile, container, false);
-
 
 
         return rootView;
