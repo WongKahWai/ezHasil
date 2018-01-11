2b5e9bacea7d6ddf094043cb9b3c04fff87d98b8
Wern Min <grayx3@hotmail.com>
2018-01-11 18:31:12 +0800

    111

--- app/src/main/java/com/example/kahwai/ezhasil/AccountActivity.java
+++ app/src/main/java/com/example/kahwai/ezhasil/AccountActivity.java
@@ -7,7 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class AccountActivity extends AppCompatActivity {
 
     private Button btnSave;
     private DatabaseReference mDatabase;
+    private String emailUser;
 
     public AccountActivity() {
     }
@@ -29,13 +37,44 @@ public class AccountActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
         View view = getSupportActionBar().getCustomView();
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-//        ft.replace(R.id.fragment_layout, new FragmentAccountEdit());
-        ft.replace(R.id.fragment_layout, new FragmentViewProfile());
-        ft.commit();
+        //Get firebase user
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        emailUser = firebaseUser.getEmail();
+
+        //Get firebase database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.child("Users").child(EncodeString(emailUser)).getValue(User.class);
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+                if(user == null)
+                    ft.replace(R.id.fragment_layout, new FragmentAccountEdit());
+                else
+                    ft.replace(R.id.fragment_layout, new FragmentViewProfile());
+
+                ft.commit();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 
+    public static String EncodeString(String string) {
+        return string.replace(".", ",");
+    }
+
+    public static String DecodeString(String string) {
+        return string.replace(",", ".");
+    }
 
 
 }
